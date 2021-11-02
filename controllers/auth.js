67b2c724363c1e1789d1b0360e7bd08c26f4d5a4
SparrowMike <michal.m_wroblewski@hotmail.com>
2021-11-02 21:04:38 +0800

    feat: 🎸 custom error handling middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,30 +14,33 @@ exports.register = async (req, res, next) => {
       user,
     });
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
+    next(error);
   }
 };
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    res
-      .status(400)
-      .json({ success: false, error: "Please provide email and password" });
+    // res
+    //   .status(400)
+    //   .json({ success: false, error: "Please provide email and password" });
+    //? Custom error
+    return next(new ErrorResponse("Please provide email and password", 400));
   }
 
   try {
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-      res.status(404).json({ success: false, error: "Invalid credential" });
+      // res.status(404).json({ success: false, error: "Invalid credential" });
+      //? Custom error
+      return next(new ErrorResponse("Invalid credential", 401));
     }
     const isMatch = await user.matchPasswords(password);
 
     if (!isMatch) {
-      res.status(404).json({ success: false, error: "Invalid credentials " });
+      // res.status(404).json({ success: false, error: "Invalid credentials " });
+      //? Custom error
+      return next(new ErrorResponse("Invalid credential", 401));
     }
     res.status(200).json({
       success: true,
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 //  Conneect DB
 connectDB();
@@ -11,7 +12,9 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 
-const PORT = process.env.PORT || 5000;
+//Error Handler ()
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000; 
 
 const server = app.listen(PORT, () =>
   console.log("Server running on port", PORT)
