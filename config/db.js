3c1f446887e7a7be6abb08053dc0e45f80fa9ab3
SparrowMike <michal.m_wroblewski@hotmail.com>
2021-11-05 13:12:37 +0800

    test: 💍 deployment

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: true,
-  });
+  await mongoose.connect(
+    process.env.MONGO_URI || "mongodb://localhost:27017/node_auth",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: true,
+    }
+  );
 
   console.log("MongoDB Connected");
 };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require('path');
 const express = require("express");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
@@ -10,12 +11,17 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/private", require("./routes/private"));
 
 //Error Handler ()
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 const server = app.listen(PORT, () =>
   console.log("Server running on port", PORT)
